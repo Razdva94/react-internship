d2a4ce7792bc8d8b74372bd1f9cd28fc56614f71
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-05-30 17:08:46 +0300

    merging completed

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,110 +4,26 @@ import SignUp from "./SignUp";
 import SignIn from "./SignIn";
 import ProtectedRouteElement from "./ProtectedRouteElement";
 import { useDispatch, useSelector } from "react-redux";
-<<<<<<< HEAD
+import Button from "@mui/material/Button";
+import Select from "@mui/material/Select";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 import ResetEmail from "./ResetEmail";
-import SendResetMessage from "./SendResetMessage";
 import { useAuth } from "../hooks/auth.hook";
 import Main from "./Main";
-=======
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-import Select from '@mui/material/Select';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-
-const HabitTracker = () => {
-  const [habits, setHabits] = useState([]);
-
-  const addHabit = () => {
-    setHabits([...habits, { name: '', frequency: '', startDate: null, endDate: null }]);
-  };
-
-  const updateHabit = (index, field, value) => {
-    const updatedHabits = [...habits];
-    updatedHabits[index][field] = value;
-    setHabits(updatedHabits);
-  };
-
-  const removeHabit = (index) => {
-    const updatedHabits = [...habits];
-    updatedHabits.splice(index, 1);
-    setHabits(updatedHabits);
-  };
-
-  const renderHabits = () => {
-    return habits.map((habit, index) => (
-      <div id='main' key={index}>
-        <Box
-          component="form"
-          sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField id="outlined-basic" label="Habit Name" variant="outlined" />
-        </Box>
-        <Select
-          value={habit.frequency}
-          onChange={(e) => updateHabit(index, 'frequency', e.target.value)}
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard-label"
-          label="Select Frequency"
-        >
-          <option value="daily">Daily</option>
-          <option value="weekly">Weekly</option>
-          <option value="monthly">Monthly</option>
-        </Select>
-        <input
-          type="date"
-          value={habit.startDate}
-          onChange={(e) => updateHabit(index, 'startDate', e.target.value)}
-        />
-        <input
-          type="date"
-          value={habit.endDate}
-          onChange={(e) => updateHabit(index, 'endDate', e.target.value)}
-        />
-        <Box
-          component="form"
-          sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField id="standard-basic" label="Description" variant="standard" />
-        </Box>
-        <Button variant="outlined" onClick={() => removeHabit(habit.id)}>Remove</Button>
-      </div>
-    ));
-  };
-
-  return (
-    <div>
-      <h1>Habit Tracker</h1>
-      {renderHabits()}
-      <Button variant="contained" onClick={addHabit}>Add Habit</Button>
-    </div>
-  );
-};
-
-export default HabitTracker;
-
->>>>>>> 87e5f07d54a6d341f9fb77e2fa9d10cedce72943
-
+import SendResetMessage from "./SendResetMessage";
+import HabitTracker from "./HabitTracker"
 function App() {
   const navigate = useNavigate();
   const { token, login, logout, userId } = useAuth();
   const signInState = useSelector((state) => state.slide.signIn);
   console.log(signInState);
-  const [authenticated, setAuthenticated ] = React.useState(false)
+  const [authenticated, setAuthenticated] = React.useState(false);
   React.useEffect(() => {
-  setAuthenticated (token)
-  }, [token])
-  console.log(authenticated)
-  
+    setAuthenticated(token);
+  }, [token]);
+  console.log(authenticated);
+
   React.useEffect(() => {
     if (authenticated) {
       navigate("/");
@@ -123,7 +39,12 @@ function App() {
         path="/"
         element={
           <ProtectedRouteElement
-            component={<Main setAuthenticated={setAuthenticated }/>}
+            component={
+              <>
+                <Main setAuthenticated={setAuthenticated} />
+                <HabitTracker />
+              </>
+            }
             loggedIn={signInState || authenticated}
           />
         }
