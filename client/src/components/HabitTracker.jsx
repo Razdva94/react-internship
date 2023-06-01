ee19e8353dbd9d3b41c6a512727592c15329cdd5
adahan7 <alea81659@gmail.com>
2023-06-01 21:29:29 +0600

    added button save

--- client/src/components/HabitTracker.jsx
+++ client/src/components/HabitTracker.jsx
@@ -6,93 +6,88 @@ import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 
 const HabitTracker = () => {
-  const [habits, setHabits] = React.useState([]);
+  const [form, setForm] = React.useState({ name: "", frequency: "daily", startDate: "", endDate: "", description: "" });
+  const [habits, setHabits] = React.useState([])
 
-  const addHabit = () => {
-    setHabits([
-      ...habits,
-      { name: "", frequency: "daily", startDate: "", endDate: "" },
-    ]);
+  const save = () => {
+    setHabits(prev => [...prev, form])
+    localStorage.setItem("habits", JSON.stringify(habits))
+    console.log(localStorage.getItem("habits"))
+
   };
-
-  const updateHabit = (index, field, value) => {
-    const updatedHabits = [...habits];
-    updatedHabits[index][field] = value;
-    setHabits(updatedHabits);
-  };
-
-  const removeHabit = (index) => {
-    const updatedHabits = [...habits];
-    updatedHabits.splice(index, 1);
-    setHabits(updatedHabits);
-  };
-
-  const renderHabits = () => {
-    return habits.map((habit, index) => (
-      <div id="main" key={index}>
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            id="outlined-basic"
-            label="Habit Name"
-            variant="outlined"
-          />
-        </Box>
-        <Select
-          value={habit.frequency}
-          onChange={(e) => updateHabit(index, "frequency", e.target.value)}
-          label="Select Frequency"
-        >
-          <MenuItem value="daily">Daily</MenuItem>
-          <MenuItem value="weekly">Weekly</MenuItem>
-          <MenuItem value="monthly">Monthly</MenuItem>
-        </Select>
-        <input
-          type="date"
-          value={habit.startDate}
-          onChange={(e) => updateHabit(index, "startDate", e.target.value)}
-        />
-        <input
-          type="date"
-          value={habit.endDate}
-          onChange={(e) => updateHabit(index, "endDate", e.target.value)}
-        />
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "25ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            id="standard-basic"
-            label="Description"
-            variant="standard"
-          />
-        </Box>
-        <Button variant="outlined" onClick={() => removeHabit(habit.id)}>
-          Remove
-        </Button>
-      </div>
-    ));
-  };
-
+  const storageHabits = JSON.parse(localStorage.getItem("habits")) || []
   return (
     <div>
       <h1>Habit Tracker</h1>
-      {renderHabits()}
-      <Button variant="contained" onClick={addHabit}>
-        Add Habit
-      </Button>
+      <div style={{ display: 'flex', }}>
+        <div>
+          <div id="main">
+            <Box
+              component="form"
+              sx={{
+                "& > :not(style)": { m: 1, width: "25ch" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="outlined-basic"
+                label="Habit Name"
+                variant="outlined"
+                onChange={(e) => setForm((prevForm) => ({ ...prevForm, name: e.target.value }))}
+
+              />
+            </Box>
+            <Select
+              value={form.frequency}
+              label="Select Frequency"
+              onChange={(e) => setForm((prevForm) => ({ ...prevForm, frequency: e.target.value }))}
+            >
+              <MenuItem value="daily">Daily</MenuItem>
+              <MenuItem value="weekly">Weekly</MenuItem>
+              <MenuItem value="monthly">Monthly</MenuItem>
+            </Select>
+            <input
+              type="date"
+              value={form.startDate}
+              onChange={(e) => setForm((prevForm) => ({ ...prevForm, startDate: e.target.value }))}
+
+            />
+            <input
+              type="date"
+              value={form.endDate}
+              onChange={(e) => setForm((prevForm) => ({ ...prevForm, endDate: e.target.value }))}
+
+            />
+            <Box
+              component="form"
+              sx={{
+                "& > :not(style)": { m: 1, width: "25ch" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="standard-basic"
+                label="Description"
+                variant="standard"
+                onChange={(e) => setForm((prevForm) => ({ ...prevForm, description: e.target.value }))}
+
+              />
+            </Box>
+          </div>
+          <Button variant="contained" onClick={save}>
+            Save
+          </Button>
+        </div>
+        <div>{storageHabits.map((habit) => (<div>{habit.name}</div>))}</div>
+      </div>
+
     </div>
+
   );
 };
 
+
+// localStorage.setItem("habits", localStorage.getItem("habits"))
 export default HabitTracker;
